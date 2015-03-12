225cb462f6e5c1ee0ae841d095d52b5fa8d0f8e8
kakadu <kakadu.hafanana@gmail.com>
2015-03-12 15:51:28 +0300

    Trying to reorganize scala projects

--- scala/project/Build.scala
+++ scala/project/Build.scala
@@ -10,7 +10,7 @@ object HelloBuild extends Build {
 
     def commonSettings = Seq(
       scalaVersion := "2.11.5",
-      resolvers ++= Seq("Sonatype Releases" at "http://oss.sonatype.org/content/repositories/releases"),
+      resolvers ++= Seq("Sonatype Releases" at "http://oss.sonatype.org/content/repositories/snapshots"),
 
       libraryDependencies ++=  Seq(
         "org.scala-lang" % "scala-compiler"  % scalaVersion.value % "provided",
@@ -30,7 +30,7 @@ object HelloBuild extends Build {
 
     lazy val root = Project(
         id = "hello",
-        base = file("."),
+        base = file("hello1"),
         settings = commonSettings
           ++ macroSettings
           ++ Seq(
@@ -45,9 +45,10 @@ object HelloBuild extends Build {
   lazy val test1 = Project(
       id = "test1",
       base = file("test1"),
+      dependencies = Seq(root % "compile->compile;test->test"),
       settings = Seq()
         ++ commonSettings
-        ++ Seq(mainClass := Some("Test1Runner"))
+        //++ Seq(mainClass := Some("Test1Runner"))
         ++ Seq (
           libraryDependencies += "org.scalatest"  % "scalatest_2.11"   % "2.2.1" % "test",
           libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.11.5" % "test",
