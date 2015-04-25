09a23ca5a8fe65fa399c77b014c2da85b35d4949
kakadu <kakadu.hafanana@gmail.com>
2015-04-25 20:25:48 +0300

    working on bench

--- caml/samples/bench.ml
+++ caml/samples/bench.ml
@@ -1,9 +1,17 @@
 open TstJSON
-open Core.Std
+open Core_bench
 open Core_bench.Std
+open Core.Std
 open Printf
 
 let () =
+  let put_result,close_results =
+    let ch = open_out "results.rez" in
+    let put ~name time = Printf.fprintf ch "%s,%s\n%!" name time in
+    let close () = Out_channel.close ch in
+    (put,close)
+  in
+
   let small_test_count = 2 in
   let small_inputs = Array.init small_test_count ~f:(fun n ->
       let filename = sprintf "../../scala/test1/src/test/resources/json%d" (n+1) in
@@ -21,36 +29,31 @@ let () =
     )
   in
 
-  Bench.(bench ~save_to_file:(fun m -> sprintf "%s.rez" (Measurement.name m) )
-           ~display_config:(Display_config.create ~don't_display_table:true ~show_percentage:false ~ascii_table:false ())
-           small_tests)
-  (* let s = *)
-  (*   let filename = "../../scala/test1/src/test/resources/json.big1" in *)
-  (*   (\* let filename = "json1" in *\) *)
-  (*   let ch = open_in filename in *)
-  (*   let ans = ExtLib.input_all ch in *)
-  (*   let ()  = close_in ch in *)
-  (*   ans *)
-  (* in *)
-  (* printf "input length: '%d'\n%!" (String.length s); *)
-  (* let open ASDF1 in *)
-  (* let stream = Lexer.create s in *)
-  (* let ans: _ ref = ref (Failed ()) in *)
-  (* let () = Core.Std.Command.run (Bench.make_command *)
-  (*   [ Bench.Test.create ~name:"Json1" (fun () -> *)
-  (*         ans := ASDF1.arr stream *)
-  (*       ) *)
-  (*   ; Bench.Test.create ~name:"Json1_orig" (fun () -> *)
-  (*         ans := ASDF1_orig.arr stream *)
-  (*       ) *)
-  (*   ] *)
-  (* ) *)
-  (* in *)
+  let meass = Bench.measure ~run_config:(Bench.Run_config.create ~time_quota:(Time.Span.of_sec 2.0) ()) small_tests in
+  let results = List.map ~f:Bench.analyze meass in
+  let results = List.filter_map results ~f:(function
+    | Error err ->
+      printf "Error %s\n%!" (Error.to_string_hum err);
+      None
+    | Ok r -> Some r)
+  in
+  Bench.display results;
+  List.iter ~f:(fun (ar: Core_bench.Analysis_result.t) ->
+      let open Core_bench.Analysis_result in
+      print_endline (name ar);
+      Array.to_list (regressions ar) |> List.filter ~f:(fun r ->
+          let s = Variable.to_string @@ Regression.responder r in
+          match s with "Time" -> true | _ -> false)
+      |> List.map ~f:(fun r ->
+          match Variable.to_string @@ Regression.responder r with
+          | "Time" ->
+            let coef = Regression.coefficients r in
+            let sum = Array.to_list coef |> List.fold_left ~init:0.0 ~f:(fun acc x -> acc +. (Coefficient.estimate x) ) in
+            Float.to_string (sum /. (Float.of_int @@ Array.length coef))
+          | _ -> failwith "some unsuported cases"
+        )
+      (* |> String.concat ~sep:"," |> print_endline; *)
+      |> (function [x] -> put_result ~name:(name ar) x | _ -> failwith "Pattern matcing should be exhaustive");
+    ) results;
 
-  (* match !ans with *)
-  (* | Parsed (_,(),_) -> printf "Parsed successfully \n%!" *)
-  (* | Failed _ -> printf "Parsing failed\n%!"; exit 1 *)
-
-
-
-(* let () = main () *)
+  close_results ()
